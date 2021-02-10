d2cf97afdf091760d7581994f68ec739d2994fc9
Luis Rosario <luisalmonter@gmail.com>
2021-02-10 17:05:18 -0500

    adds pages to app

---
+++ src/pages/SearchPage.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function SearchPage(props) {
+    return (
+        <div className="SearchPage">
+            <h1>search</h1>
+        </div>
+    );
+}
+
+export default SearchPage;
